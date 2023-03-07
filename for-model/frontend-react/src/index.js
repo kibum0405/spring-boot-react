dbffda9fe6e277dfce3b2db00b19d6319473f87b
kibumpark <top5w5@naver.com>
2023-03-07 14:23:06 +0900

    a

--- for-model/frontend-react/src/index.js
+++ for-model/frontend-react/src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {HashRouter} from 'react-router-dom';
+import axios from 'axios'
+
+
+axios.backend = null; 
+
+axios.fixUrl = function(original) {
+
+  if(!axios.backend && original.indexOf("/")===0) return original;
+  let url = null;
+  try {
+    url = new URL(original);
+  }catch(e){
+    url = new URL(axios.backend + original);
+  }
+
+  if(!axios.backend) return url.pathname;
+
+  url.hostname = axios.backendUrl.hostname;
+  url.port = axios.backendUrl.port;
+
+  return url.href;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,8 +33,3 @@ root.render(
     </HashRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
